8704867d258058af625c40e2eecacb5aecbb895c
aliyun4384871743 <1253576153@qq.com>
2022-05-14 18:30:08 +0800

    update(基础接口版本): 修改sql文件以及数据字典

--- 数据库设计/基础接口版本/user.sql
+++ 数据库设计/基础接口版本/user.sql
@@ -1,17 +1,17 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : LocalMySQL
+ Source Server         : Tiktok
  Source Server Type    : MySQL
- Source Server Version : 80029
- Source Host           : localhost:3306
- Source Schema         : titoktest
+ Source Server Version : 80027
+ Source Host           : 81.70.17.190:3306
+ Source Schema         : Tiktok
 
  Target Server Type    : MySQL
- Target Server Version : 80029
+ Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 10/05/2022 22:34:16
+ Date: 14/05/2022 12:46:12
 */
 
 SET NAMES utf8mb4;
@@ -22,17 +22,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `user_id` int NOT NULL COMMENT '用户id，唯一，标识用户',
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户昵称',
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户账号',
-  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户密码',
-  `follow_count` int NULL DEFAULT NULL COMMENT '用户关注总数',
-  `follower_count` int NULL DEFAULT NULL COMMENT '用户粉丝总数',
-  PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
+  `user_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户id，唯一标识',
+  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '用户名，唯一，登陆时使用，也是用户昵称',
+  `password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '登录密码',
+  PRIMARY KEY (`user_id`) USING BTREE,
+  UNIQUE INDEX `index_name`(`name`) USING BTREE COMMENT '加速查询用户名，用于登录与注册查询用户名是否存在'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
