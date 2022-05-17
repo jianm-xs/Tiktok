cf4b689bf858b323355b4fa9286dfc3d741f33df
kiceAmber <kiceamber@gmail.com>
2022-05-17 22:03:29 +0800

    feat(controller/user.go):实现了登录接口

--- Project/controller/user.go
+++ Project/controller/user.go
@@ -5,6 +5,7 @@
 package controller
 
 import (
+	"Project/models"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -95,14 +96,98 @@ func RefreshToken(tokenStr string) (string, error) {
 //*****************************************************************************************************
 
 type UserLoginResponse struct {
-	Response        // 状态码、状态描述
+	models.Response        // 状态码、状态描述
 	UserId   int64  `json:"user_id,omitempty"` // 用户id
 	Token    string `json:"token"`             // 鉴权
 }
 
 // Login 用户登录接口
 func Login(c *gin.Context) {
+	var result UserLoginResponse //登录状态码，默认 0 为成功，其他为失败
 
+	//获取用户名和密码
+	userName := c.Query("username")
+	userPassword := c.Query("password")
+
+	//数据库连接P
+	db, _ := sql.Open("mysql", "root:root@tcp(81.70.17.190:3306)/Tiktok")
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			result.Response.StatusCode = -1 // 更改状态码
+			result.Response.StatusMsg = "Close database error!"
+			result.UserId = -1
+			result.Token = ""
+			c.JSON(http.StatusOK, result) // 设置返回的信息
+			return
+		}
+	}(db) // 使用完毕后关闭数据库
+
+	err := db.Ping() //连接数据库
+
+	if err != nil {
+		result.Response.StatusCode = -2
+		result.Response.StatusMsg = "Connect database error!"
+		result.UserId = -1
+		result.Token = ""
+		c.JSON(http.StatusOK, result)
+		return
+	}
+
+	//查询数据库username是否存在
+	ans, err := db.Query("SELECT name FROM user WHERE name=?;", userName) // 查询user表中username值为用户所输入的
+	// 保存查询结果
+	if err != nil {
+		result.Response.StatusCode = -3
+		result.Response.StatusMsg = "Read username error!"
+		result.UserId = -1
+		result.Token = ""
+		c.JSON(http.StatusOK, result)
+		return
+	}
+
+	var userId int64
+	var name string
+	var password string
+
+	for ans.Next() {
+		err := ans.Scan(&name) //获取查询结果
+		if err != nil {
+			result.Response.StatusCode = -4
+			result.Response.StatusMsg = "Read username error!"
+			result.UserId = -1
+			result.Token = ""
+			c.JSON(http.StatusOK, result)
+			return
+		}
+	}
+
+	//查询数据库中的所有数据
+	queryCommand := "SELECT user_id, name, password FROM user where name = ? and password = ?"
+	rows, _ := db.Query(queryCommand, userName, userPassword)
+
+	for rows.Next() {
+		err := rows.Scan(&userId, &name, &password)
+		if err != nil { //如果查询失败
+			result.StatusCode = -5
+			result.StatusMsg = "query failed!"
+			result.UserId = -1
+			result.Token = ""
+			c.JSON(http.StatusOK, result)
+			fmt.Println("失败3")
+			return
+		}
+
+		tokenStr, _ := GenToken(string(userId))
+
+		//用户名和密码同时匹配成功就返回id和token
+		result.StatusCode = 0
+		result.StatusMsg = "success"
+		result.UserId = userId
+		result.Token = tokenStr
+		c.JSON(http.StatusOK, result)
+		return
+	}
 }
 
 // Register 用户注册接口
