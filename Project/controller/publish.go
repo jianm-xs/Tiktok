1891672bc023647bd701d95b860334699553e80e
jianm-xs <1143497246@qq.com>
2022-05-26 12:03:07 +0800

    perf(publish): 完善了发布列表接口，并将数据库操作迁到了 Dao 中

--- Project/controller/publish.go
+++ Project/controller/publish.go
@@ -10,7 +10,6 @@ import (
 	"Project/models"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/jmoiron/sqlx"
 	"net/http"
 	"path"
 	"path/filepath"
@@ -107,72 +106,34 @@ type VideoVo struct {
 	IsFavorite    bool   `json:"is_favorite" db:"isfavourite"`        // 是否已点赞
 }
 
-var Db *sqlx.DB
-
-//通过token解析后获取用户信息，根据该信息查询数据库中的用户视频列表，封装在一起后返回
-//PS:	token解析还没写，现在当做没有token解析这步
-func getUserVideoInfoByToken(token string) []models.Video {
-
-	//初始化数据库连接
-	database, err := sqlx.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
-	//若数据库连接失败，报错，返回空
-	if err != nil {
-		fmt.Println("连接数据库失败：" + err.Error())
-		return []models.Video{}
-	}
-	//连接成功后赋值
-	Db = database
-	var videos []models.Video //用户视频列表，即最终返回值
-	var videoVos []VideoVo    //视频列表信息，用来接收数据库传来的值
-	var user []models.User    //用户信息，go的DB操作要求数据库查出来必须用数组类型接收，大概吧，所以就用这个，
-
-	//从数据库查视频信息，用videoVos接收数据
-	err = Db.Select(&videoVos, "select * from video where user_id=?", 1)
-	//如果查询失败，报错，返回空
-	if err != nil {
-		fmt.Println("exec failed, ", err)
-		return []models.Video{}
-	}
-	//从数据库查用户信息，这里应该是根据token解析出来的信息来查用户所有信息，还没写token
-	err = Db.Select(&user, "select * from user where id=?", 1)
-	//如果查询失败，报错，返回空
-	if err != nil {
-		fmt.Println("exec failed, ", err)
-		return []models.Video{}
-	}
-
-	//关闭数据库连接，必须卸载err之后的下面，我也不知道为什么，反正文档是这么说的
-	defer Db.Close()
-
-	//封装返回值videos，即将视频信息videoVos和用户信息user合并成要求的形式返回
-	for _, v := range videoVos {
-		t := models.Video{
-			ID:            v.Id,
-			Author:        user[0],
-			PlayUrl:       v.PlayUrl,
-			CoverUrl:      v.CoverUrl,
-			FavoriteCount: v.FavoriteCount,
-			CommentCount:  v.CommentCount,
-			IsFavorite:    v.IsFavorite,
-		}
-		videos = append(videos, t)
-	}
-
-	for _, v := range videos {
-		fmt.Println(v)
-	}
-
-	//over
-	return videos
-}
-
-// PublishList 真正的 PulishList   controller
+// PublishList 视频发布列表
 func PublishList(c *gin.Context) {
-	// 声明接收的变量
-	var token string = "qqq"
+	// 返回的结果
+	var result models.VideoListResponse
+	// 获取请求的 token
+	token := c.DefaultQuery("token", "")
+	// 获取作者的 id
+	authorId, err := strconv.ParseInt(c.DefaultQuery("user_id", "-1"), 10, 64)
+	if err != nil {
+		result.StatusCode = -1
+		result.StatusMsg = "get author id error!"
+		c.JSON(http.StatusOK, result)
+		return
+	}
+	// 获取当前用户的 id
+	var userId int64
+	myClaims, err := ParseToken(token)
+	if err != nil { // token 解析失败
+		result.StatusCode = -2            // 失败，设置状态码和描述
+		result.StatusMsg = "token error!" // token 有误
+		c.JSON(http.StatusOK, result)     // 设置返回的信息
+		return
+	} else { // 如果 token 解析成功，获取 userId
+		userId, _ = strconv.ParseInt(myClaims.Uid, 10, 64)
+	}
 
-	//获取发布视频信息
-	videos := getUserVideoInfoByToken(token)
+	//获取 author 发布视频信息，userId 用于判断是否关注了
+	videos := dao.GetVideoList(authorId, userId)
 
 	//接口返回
 	c.JSON(http.StatusOK, models.VideoListResponse{
