7e6d83d43de933b11bbd10a7aa0e731574a344f4
jianm-xs <1143497246@qq.com>
2022-05-29 17:50:21 +0800

    fix(favorite & relation): 因接口文档变动修改请求参数

--- Project/controller/favorite.go
+++ Project/controller/favorite.go
@@ -11,29 +11,30 @@ import (
 // FavoriteAction 点赞接口
 func FavoriteAction(c *gin.Context) {
 	// 获取请求参数
-	uid := c.DefaultQuery("user_id", "-1")
-	userId, _ := strconv.ParseInt(uid, 10, 64)
 	token := c.DefaultQuery("token", "")
 	videoId, _ := strconv.ParseInt(c.DefaultQuery("video_id", "-1"), 10, 64)
 	actionType, _ := strconv.ParseInt(c.DefaultQuery("action_type", "-1"), 10, 64)
 	// 参数获取失败
-	if userId == -1 || token == "" || videoId == -1 || actionType == -1 {
+	if token == "" || videoId == -1 || actionType == -1 {
 		c.JSON(http.StatusOK, models.Response{
 			StatusCode: -1,
 			StatusMsg:  "failed to obtain parameters!",
 		})
 		return
 	}
-	// token 校验失败
-	if answer, err := CheckToken(token, uid); err != nil || answer == false {
+	var userId int64
+	myClaims, err := ParseToken(token)
+	if err != nil { // token 解析失败
 		c.JSON(http.StatusOK, models.Response{
 			StatusCode: -2,
 			StatusMsg:  "token error!",
 		})
 		return
+	} else { // 如果 token 解析成功，获取 userId
+		userId, _ = strconv.ParseInt(myClaims.Uid, 10, 64)
 	}
 	// 数据库操作
-	err := dao.FavoriteAction(userId, videoId, actionType)
+	err = dao.FavoriteAction(userId, videoId, actionType)
 	if err != nil {
 		// 操作失败
 		c.JSON(http.StatusOK, models.Response{
