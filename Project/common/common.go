7492b57c922e8a095c22c560cd9dc7a7b040bb36
jianm-xs <1143497246@qq.com>
2022-06-12 16:23:38 +0800

    change: 对代码中的魔法数字进行了统一修改

--- Project/common/common.go
+++ Project/common/common.go
@@ -7,5 +7,9 @@ package common
 // 状态相关常量定义
 
 const (
-	StatusOK = 0
+	StatusOK     = 0  // 状态成功
+	StatusQuery  = -1 // 获取参数错误
+	StatusData   = -2 // 数据操作失败
+	StatusToken  = -3 // Token解析失败
+	StatusOption = -4 // 无效请求
 )
